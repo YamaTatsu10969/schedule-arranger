4c08a6d6bcc8d283bbbec2c5b6bcdcd27c367f0e
YamaTatsu10969 <52.wadachi.96@gmail.com>
2021-05-11 21:18:07 +0900

    add models

--- app.js
+++ app.js
@@ -10,6 +10,24 @@ var helmet = require('helmet');
 var session = require('express-session');
 var passport = require('passport');
 
+// read models
+var User = require('./models/user');
+var Schedule = require('./models/schedule');
+var Availability = require('./models/availability');
+var Candidate = require('./models/candidate');
+var Comment = require('./models/comment');
+User.sync().then(() => {
+  Schedule.belongsTo(User, { foreignKey: 'createdBy' });
+  Schedule.sync();
+  Comment.belongsTo(User, { foreignKey: 'userId' });
+  Comment.sync();
+  Availability.belongsTo(User, { foreignKey: 'userId' });
+  Candidate.sync().then(() => {
+    Availability.belongsTo(Candidate, { foreignKey: 'candidateId' });
+    Availability.sync();
+  });
+});
+
 var GitHubStrategy = require('passport-github2').Strategy;
 
 // TODO(yamatatsu): change secret file
@@ -33,7 +51,12 @@ passport.use(
     },
     function (accessToken, refreshToken, profile, done) {
       process.nextTick(function () {
-        return done(null, profile);
+        User.upsert({
+          userId: profile.id,
+          username: profile.username,
+        }).then(() => {
+          done(null, profile);
+        });
       });
     },
   ),
