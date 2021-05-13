cfe8d21790357699728cdd03b2058451709af391
YamaTatsu10969 <52.wadachi.96@gmail.com>
2021-05-14 06:56:23 +0900

    fix

--- models/sequelize-loader.js
+++ models/sequelize-loader.js
@@ -3,7 +3,12 @@ const { Sequelize, DataTypes } = require('sequelize');
 
 const sequelize = new Sequelize(
   process.env.DATABASE_URL ||
-    'postgres://postgres:postgres@db/schedule_arranger',
+    'postgres://postgres:postgres@db/schedule_arranger', dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    }
+  },
 );
 
 module.exports = {
