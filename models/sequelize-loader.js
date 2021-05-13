6cb2a1ab7eb960c44b093f1f45488350aab83055
YamaTatsu10969 <52.wadachi.96@gmail.com>
2021-05-14 07:31:52 +0900

    fix

--- models/sequelize-loader.js
+++ models/sequelize-loader.js
@@ -1,25 +1,26 @@
 'use strict';
 const { Sequelize, DataTypes } = require('sequelize');
 
-const sequelize = new Sequelize(
-  process.env.DATABASE_URL ||
-    'postgres://postgres:postgres@db/schedule_arranger',
-);
 // const sequelize = new Sequelize(
 //   process.env.DATABASE_URL ||
 //     'postgres://postgres:postgres@db/schedule_arranger',
-//   'postgres',
-//   'postgres',
-//   {
-//     dialect: 'postgres',
-//     dialectOptions: {
-//       ssl: {
-//         require: true,
-//         rejectUnauthorized: false,
-//       },
-//     },
-//   },
 // );
+const sequelize = new Sequelize(
+  process.env.DATABASE_URL ||
+    'postgres://postgres:postgres@db/schedule_arranger',
+  {
+    database: 'schedule_arranger',
+    username: 'postgres',
+    password: 'postgres',
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  },
+);
 
 module.exports = {
   sequelize,
