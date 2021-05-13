2eb064cf4b5cdf49aa221142a2bfa26c9e799ae6
YamaTatsu10969 <52.wadachi.96@gmail.com>
2021-05-13 20:11:27 +0900

    add heroku settings

--- models/sequelize-loader.js
+++ models/sequelize-loader.js
@@ -1,7 +1,8 @@
 'use strict';
 const { Sequelize, DataTypes } = require('sequelize');
 const sequelize = new Sequelize(
-  'postgres://postgres:postgres@db/schedule_arranger',
+  process.env.DATABASE_URL ||
+    'postgres://postgres:postgres@localhost/schedule_arranger',
 );
 
 module.exports = {
